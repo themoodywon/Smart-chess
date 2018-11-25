e05fa49147e614004454dbc624b966627e53fd44
Sam Moody Wong <themoodywon@tamu.edu>
2018-11-25 16:15:19 -0600

    innitial commit

---
+++ smartchess.py
@@ -0,0 +1,17 @@
+import chess
+import gpiozero
+import time
+
+#pin assignment
+
+
+
+#get the state of the board
+def poll_board():
+    for i in range(0,7):
+        led
+    
+
+
+def main():
+    
